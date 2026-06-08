cc49fcaa46544d406695d593b21a9741610df855
fizakim <fizakim@gmail.com>
2026-06-08 17:09:09 +0100

    move main.py from root to enigma folder

--- enigma/main.py
+++ enigma/main.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
+
 from config.config3 import config3
 
 positions = [0, 0, 0]
@@ -12,4 +16,3 @@ print("Ciphertext:", ciphertext)
 machine_dec = config3.build(positions)
 decrypted = machine_dec.decrypt(ciphertext)
 print("Decrypted: ", decrypted)
-
