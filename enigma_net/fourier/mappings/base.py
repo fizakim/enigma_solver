4119080c260689d67ccf2474be505354294aaa7d
fizakim <fizakim@gmail.com>
2026-06-25 22:11:00 +0100

    code refactor

--- enigma_net/fourier/mappings/base.py
+++
@@ -1,9 +0,0 @@
-import torch.nn as nn
-
-class VectorMatrixMapping(nn.Module):
-    def __init__(self, size):
-        super().__init__()
-        self.n = size
-
-    def forward(self, perm_values, tau):
-        raise NotImplementedError
